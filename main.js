ef089b54f4d705dd19e683a683c8f16e0a8efec1
pedro <pcnahime12@gmail.com>
2017-12-10 18:20:26 -0200

    bruteForce arrumado

--- main.js
+++ main.js
@@ -10,30 +10,33 @@ for (var i = 0; i < 4; i++) {
     senha.push(aleatorio(0, 9));
 }
 
-function resetaSenha(){
+function resetaSenha() {
     senha = [];
     tentativas = 0
 }
-
-var descobrirSenha = function (a, b, c, d) {
+var descobrirSenha = function (tentativa) {
+    tentativa = tentativa.toString();
+    
+    array = tentativa.split('');
+    
     var retorno = '';
     var erro = 0;
     while (erro == 0) {
-        if (senha[0] != a) {
+        if (senha[0] != array[0]) {
             retorno += "senha incorreta";
             erro++;
         } else {
-            if (a == senha[0]) {
+            if (array[0] == senha[0]) {
                 retorno += 'tente outra vez*';
-                if (b == senha[1]) {
-                    
+                if (array[1] == senha[1]) {
+
                     retorno += '*';
-                    if (c == senha[2]) {
-                        
+                    if (array[2] == senha[2]) {
+
                         retorno += '*';
-                        if (d == senha[3]) {
-                            console.log("aqui");
-                            retorno = 'você acertou!';
+                        if (array[3] == senha[3]) {
+                        
+                            retorno = 'acertou!';
                             senha = [];
                             tentativas = 0
                             erro++
@@ -41,7 +44,7 @@ var descobrirSenha = function (a, b, c, d) {
                             erro++;
                         }
                     } else {
-                    
+
                         erro++;
                     }
                 } else {
@@ -50,7 +53,37 @@ var descobrirSenha = function (a, b, c, d) {
             }
         }
     }
-    console.log(retorno);
     return retorno;
 }
-descobrirSenha(1, 2 ,3 ,4);
+
+
+var bruteForce = function () {
+    var tentativasErro = [];
+    var acerto = ''
+
+    geraNumeroParaSenha = function () {
+        var um = Math.floor(Math.random() * 9) + 0;
+        var dois = Math.floor(Math.random() * 9) + 0;
+        var tres = Math.floor(Math.random() * 9) + 0;
+        var quatro = Math.floor(Math.random() * 9) + 0;
+        var numero = '' + um + dois + tres + quatro;
+        return numero
+    }
+    while (acerto != "acertou!") {
+        tentativa = geraNumeroParaSenha()
+        var existe = 0
+        for (var i = 0; i < tentativasErro.length; i++) {
+            if (tentativasErro[i] == tentativa) {
+                existe++
+            }
+        }
+
+        if (existe == 0) {
+            acerto = descobrirSenha(tentativa)
+            tentativasErro.push(tentativa);
+        }
+    }
+console.log("senha: " + tentativa);
+console.log("numero de tentativas = " + tentativasErro.length)
+}
+bruteForce()
